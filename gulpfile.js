851d25f8b4a6f8a2fb20ac475bc22bc2bf28fbee
Roger Campos <rcampos@media703.co>
2022-09-13 11:14:58 -0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+// list dependencies
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const prefix = require('gulp-autoprefixer');
+const minify = require('gulp-clean-css');
+const terser = require('gulp-terser');
+
+
+// create funtions
+
+//scss
+function compilescss() {
+    return src('src/scss/*.scss')
+        .pipe(sass())
+        .pipe(prefix('last 2 versions'))
+        .pipe(minify())
+        .pipe(dest('dist/css'))
+}
+
+//js
+function jsmin() {
+    return src('src/js/*.js')
+        .pipe(terser())
+        .pipe(dest('dist/js'))
+}
+
+// create watch task
+function watchTask(){
+    watch('src/scss/**/*.scss', compilescss); 
+    watch('src/js/*.js', jsmin); 
+}
+
+// default gulp
+exports.default = series(
+    compilescss,
+    jsmin,
+    watchTask
+  );
